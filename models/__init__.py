4951559d657a19a595b5d916ea3b795779c16bc3
loda26 <mokhaled26498@gmail.com>
2024-04-21 15:12:28 +0200

    loda

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
+
+from os import getenv
+
+storage_t = getenv("FITAI_TYPE_STORAGE")
---
+++ models/user.py
@@ -0,0 +1,37 @@
+#!/use/bin/python3
+"""
+class user
+"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+
+
+class User(BaseException, Base):
+    """
+    class of user
+    """
+    if models.storage_t == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        name = Column(String(128), nullable=True)
+        age = Column(String(128), nullable=True)
+        gender = Column(String(128), nullable=True)
+        weight = Column(String(128), nullable=True)
+        height = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        name = ""
+        age = ""
+        gender = ""
+        weight = ""
+        height = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
